7134990247c5a75f5d18e751d6880c65fcec1c79
tsekeres <twsekeres@gmail.com>
2021-07-23 21:46:20 -0500

    Add project files.

---
+++ Favorite things/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Favorite_things
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Hotspurs = new Soccer
+            {
+                JerseyColor = "Navy Blue and White",
+                GoalsFor = 6,
+                GoalsAgainst = 3
+            };
+
+            var SixPack = new Soccer
+            {
+                JerseyColor = "Red and Black",
+                GoalsFor = 2,
+                GoalsAgainst = 10
+            };
+
+            Console.WriteLine("Welcome to our team!");
+            Hotspurs.Colors();
+
+            Hotspurs.Score();
+        }
+    }
+}
---
+++ Favorite things/Soccer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Favorite_things
+{
+    class Soccer
+    {
+        public string JerseyColor { get; set; }
+        public int GoalsFor { get; set; }
+        public int GoalsAgainst { get; set; }
+
+        public void Score()
+        {
+            Console.WriteLine($"The score is {GoalsFor} to {GoalsAgainst}.");
+        }
+
+        public void Colors()
+        {
+            Console.WriteLine($"Our jerseys are {JerseyColor}.");
+        }
+    }
+}
